5ab73e13204fce5a279e583545c079d079973e22
Nikku <161804431+nikkuinutori@users.noreply.github.com>
2024-03-02 12:08:49 +0200

    Huhuu Media Redesign Project

---
+++ huhuu-media-redesign-project/huhuu-media-main-page/js/scroll-up.js
@@ -0,0 +1,5 @@
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
